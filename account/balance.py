89af219b36c8158a766e6fefc9666d4c1efac0b0
CThaw90 <cthaw@mediaocean.com>
2025-12-14 22:05:19 -0500

    Converting lambda response from dict to string

--- account/balance.py
+++ account/balance.py
@@ -1,5 +1,7 @@
 from common.logger import Logger
 
+import json
+
 logger = Logger('account.balance')
 
 def main(args, context):
@@ -9,4 +11,4 @@ def main(args, context):
     logger.info('Lambda function was invoked with the following context...')
     logger.info(context)
 
-    return {'statusCode': 200, 'body': {'coins': 0, 'tokens': 0, 'pending': 0}}
+    return {'statusCode': 200, 'body': json.dumps({'coins': 0, 'tokens': 0, 'pending': 0})}
